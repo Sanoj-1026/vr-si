75eddb33122e4b4dcb69ad58e7c82bbc773ecc0e
Sanoj-1026 <146769232+Sanoj-1026@users.noreply.github.com>
2024-08-22 14:58:41 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ function Home() {
         <img src={logo} alt="Logo" className="logo" />
         <nav className="navbar">
           <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
+            <li><a href="#">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#service">Service</a></li>
             <li><a href="#clients">Clients</a></li>
